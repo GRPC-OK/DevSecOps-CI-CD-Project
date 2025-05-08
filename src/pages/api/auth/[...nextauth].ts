ac12788642e058616afc4c64b53d479ab416fec3
kimsunwoo <ksw5033@naver.com>
2025-05-08 14:42:00 +0900

    웹 서버 github 소셜로그인 기능 버그 수정 완료

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,6 +10,18 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+
+  // 세션을 지속 쿠키로 저장
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 60, // 30분 (단위: 초)
+    updateAge: 5 * 60,   // 사용자가 활동하면 5분 간격으로 세션 갱신
+  },
+
+  // 선택적으로 JWT 설정도 명시 가능
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
 });
 
 
